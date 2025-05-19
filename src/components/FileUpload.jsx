36fb9d85779e0cbc3293f1d049857be84a321495
Muhammad Hashim <muhammad.hashimsid@gmail.com>
2025-05-19 05:45:30 +0500

    Updated Dashboard

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -70,7 +70,7 @@ export default function FileUploadWithDropdown() {
         </div>
         
       )}
-      <button>Upload</button>
+      <button className={styles.button} >Upload</button>
     </div>
   );
 }
